9d4f5a8a7f5b607de76cbab6c5a7797e0c3b4a56
henriqueconte <henriqueconte6@gmail.com>
2021-11-23 14:41:54 +0100

    Implemented local word counter using map reduce

---
+++ WordCount/mapper.py
@@ -0,0 +1,8 @@
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    words_list = line.split()
+
+    for word in words_list:
+        print(word + "\t" + str(1))
---
+++ WordCount/reducer.py
@@ -0,0 +1,15 @@
+import sys
+
+word_count = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    word, count = line.split('\t', 1)
+    try:
+        count = int(count)
+        word_count += count
+    except:
+        pass
+
+print (word_count)
+
